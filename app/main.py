1653bf9de695f7ffc5061c3fd82585cfa2e42b93
makoto hata <makotohata0429@gmail.com>
2024-09-18 08:09:24 +0900

    item find_all

--- app/main.py
+++ app/main.py
@@ -1,15 +1,10 @@
-from typing import Union
-
 from fastapi import FastAPI
+from cruds import item as item_cruds
+
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items")
+async def find_all():
+    return item_cruds.find_all()
