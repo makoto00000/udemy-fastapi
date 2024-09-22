260fb36b4ed96a3ff003512c02dddd2bba429b78
makoto hata <makotohata0429@gmail.com>
2024-09-22 11:32:29 +0900

    status code

--- app/routers/item.py
+++ app/routers/item.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Path, Query, HTTPException
+from starlette import status
 from cruds import item as item_cruds
 from schemas import ItemCreate, ItemUpdate, ItemResponse
 
@@ -6,12 +7,14 @@ from schemas import ItemCreate, ItemUpdate, ItemResponse
 router = APIRouter(prefix="/items", tags=["Items"])
 
 
-@router.get("", response_model=list[ItemResponse])
+@router.get("", response_model=list[ItemResponse],
+            status_code=status.HTTP_200_OK)
 async def find_all():
     return item_cruds.find_all()
 
 
-@router.get("/{id}", response_model=ItemResponse)
+@router.get("/{id}", response_model=ItemResponse,
+            status_code=status.HTTP_200_OK)
 async def find_by_id(id: int = Path(gt=0)):
     found_item = item_cruds.find_by_id(id)
     if not found_item:
@@ -19,17 +22,20 @@ async def find_by_id(id: int = Path(gt=0)):
     return found_item
 
 
-@router.get("/", response_model=list[ItemResponse])
+@router.get("/", response_model=list[ItemResponse],
+            status_code=status.HTTP_200_OK)
 async def find_by_name(name: str = Query(min_length=2, max_length=20)):
     return item_cruds.find_by_name(name)
 
 
-@router.post("", response_model=ItemResponse)
+@router.post("", response_model=ItemResponse,
+             status_code=status.HTTP_201_CREATED)
 async def create(item_create: ItemCreate):
     return item_cruds.create(item_create)
 
 
-@router.put("/{id}", response_model=ItemResponse)
+@router.put("/{id}", response_model=ItemResponse,
+            status_code=status.HTTP_200_OK)
 async def update(item_update: ItemUpdate, id: int = Path(gt=0)):
     updated_item = item_cruds.update(id, item_update)
     if not updated_item:
@@ -37,7 +43,8 @@ async def update(item_update: ItemUpdate, id: int = Path(gt=0)):
     return updated_item
 
 
-@router.delete("/{id}", response_model=ItemResponse)
+@router.delete("/{id}", response_model=ItemResponse,
+               status_code=status.HTTP_200_OK)
 async def delete(id: int = Path(gt=0)):
     deleted_item = item_cruds.delete(id)
     if not deleted_item:
