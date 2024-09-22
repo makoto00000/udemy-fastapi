4c6114cf6d8b8e5be4702794e5d3e7c0b01e20e5
makoto hata <makotohata0429@gmail.com>
2024-09-22 10:00:30 +0900

    HTTPException

--- app/routers/item.py
+++ app/routers/item.py
@@ -1,5 +1,4 @@
-from typing import Optional
-from fastapi import APIRouter, Path, Query
+from fastapi import APIRouter, Path, Query, HTTPException
 from cruds import item as item_cruds
 from schemas import ItemCreate, ItemUpdate, ItemResponse
 
@@ -12,9 +11,12 @@ async def find_all():
     return item_cruds.find_all()
 
 
-@router.get("{id}", response_model=Optional[ItemResponse])
+@router.get("/{id}", response_model=ItemResponse)
 async def find_by_id(id: int = Path(gt=0)):
-    return item_cruds.find_by_id(id)
+    found_item = item_cruds.find_by_id(id)
+    if not found_item:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return found_item
 
 
 @router.get("/", response_model=list[ItemResponse])
@@ -27,11 +29,17 @@ async def create(item_create: ItemCreate):
     return item_cruds.create(item_create)
 
 
-@router.put("/{id}", response_model=Optional[ItemResponse])
+@router.put("/{id}", response_model=ItemResponse)
 async def update(item_update: ItemUpdate, id: int = Path(gt=0)):
-    return item_cruds.update(id, item_update)
+    updated_item = item_cruds.update(id, item_update)
+    if not updated_item:
+        raise HTTPException(status_code=404, detail="Item not updated")
+    return updated_item
 
 
-@router.delete("/{id}", response_model=Optional[ItemResponse])
+@router.delete("/{id}", response_model=ItemResponse)
 async def delete(id: int = Path(gt=0)):
-    return item_cruds.delete(id)
+    deleted_item = item_cruds.delete(id)
+    if not deleted_item:
+        raise HTTPException(status_code=404, detail="Item not deleted")
+    return deleted_item
